06ce3a6daede497dfefb571dae80e95fd8cd156d
Israel Glixinski <israel_glixinski@hotmail.com>
2025-04-29 21:46:49 -0300

    .

--- trancrever.py
+++ trancrever.py
@@ -3,13 +3,15 @@ import whisper
 model = whisper.load_model("base")  
 
 
-def transcrever(filename="gravacao_sistema.wav"):
+def transcrever(filename="1.wav"):
     print('\n')
 
     try: 
         result = model.transcribe(filename)
         print(f'{result["text"]}')
-        Thread(target=lambda:traduzir(result["text"])).start()
     except: 
         print(f"Não foi possivel transcrever \n")
 
+
+
+transcrever()
